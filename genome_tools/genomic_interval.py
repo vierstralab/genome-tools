e6d46d410c469a8e2ce47d663e116c1ce5da6729
wishabc <aswq22013@gmail.com>
2024-11-06 16:53:58 -0800

    added genome tools method

--- genome_tools/genomic_interval.py
+++ genome_tools/genomic_interval.py
@@ -1,6 +1,7 @@
 # Copyright 2015 Jeff Vierstra
 
-import numpy as np
+from typing import List
+import pandas as pd
 
 
 class GenomicInterval(object):
@@ -13,9 +14,10 @@ class GenomicInterval(object):
         self.start = int(start)
         self.end = int(end)
         self.name = str(name)
-        self._extra_fields = []
+        self.req_fields = ["chrom", "start", "end", "name"]
+        self.extra_fields = []
         for key, value in kwargs.items():
-            self._extra_fields.append(key)
+            self.extra_fields.append(key)
             setattr(self, key, value)
 
     def from_ucsc(ucsc_str):
@@ -32,7 +34,7 @@ class GenomicInterval(object):
         """Returns a string-formated version of the element
         for printing
         """
-        extra_fields = f", extra_fields={tuple(self._extra_fields)}" if len(self._extra_fields) > 0 else ""
+        extra_fields = f", extra_fields={tuple(self.extra_fields)}" if len(self.extra_fields) > 0 else ""
         return f'{self.__class__.__name__}({self.to_ucsc()}{extra_fields})'
 
     def to_ucsc(self):
@@ -42,8 +44,28 @@ class GenomicInterval(object):
         return f"{self.chrom}:{self.start}-{self.end}"
     
     @property
-    def _extra_kwargs(self):
-        return {key: getattr(self, key) for key in self._extra_fields}
+    def extra_kwargs(self):
+        return {key: getattr(self, key) for key in self.extra_fields}
+    
+    def __add__(self, x: int):
+        if not isinstance(x, int):
+            raise TypeError("shift must be an integer")
+        return self.shift(x)
+    
+    def __sub__(self, x: int):
+        if not isinstance(x, int):
+            raise TypeError("shift must be an integer")
+        return self.shift(-x)
+    
+    def __mul__(self, x: float):
+        if not isinstance(x, (int, float)):
+            raise TypeError("zoom-in factor must be an int or float")
+        return self.zoom(x)
+    
+    def __truediv__(self, x: float):
+        if not isinstance(x, (int, float)):
+            raise TypeError("zoom-out factor must be an int or float")
+        return self.zoom(1 / x)
 
     def widen(self, x, inplace=False):
         """Expands the coordinates"""
@@ -58,7 +80,7 @@ class GenomicInterval(object):
             self.end = new_end
             return self
         return GenomicInterval(
-            self.chrom, new_start, new_end, self.name, **self._extra_kwargs
+            self.chrom, new_start, new_end, self.name, **self.extra_kwargs
         )
     
     def zoom(self, zoom_factor, inplace=False):
@@ -81,7 +103,7 @@ class GenomicInterval(object):
             self.end += x
             return self
         return GenomicInterval(
-            self.chrom, self.start + x, self.end + x, self.name, **self._extra_kwargs
+            self.chrom, self.start + x, self.end + x, self.name, **self.extra_kwargs
         )
     
 
@@ -91,8 +113,9 @@ class VariantInterval(GenomicInterval):
         super().__init__(chrom, start, end, **kwargs)
         self.ref = ref
         self.alt = alt
-        self.pos = end
         self.value = value
+        self.req_fields += ["ref", "alt", "value"]
+        self.pos = end
 
     def to_ucsc(self):
         return f"{self.chrom}:{self.pos}:{self.ref}:{self.alt}"
@@ -107,28 +130,53 @@ class VariantInterval(GenomicInterval):
         raise NotImplementedError("Cannot zoom a variant interval")
 
 
-def filter_df_to_interval(df, interval):
+def filter_df_to_interval(df: pd.DataFrame, interval: GenomicInterval):
     chromosome_col = 'chrom' if 'chrom' in df.columns else '#chr'
     return df.loc[df[chromosome_col] == interval.chrom].query(
         f'end >= {interval.start} & start < {interval.end}'
     )
 
 
-def df_to_genomic_intervals(df, interval=None, extra_columns=()):
+def _sanitize_df(df: pd.DataFrame, interval: GenomicInterval):
     df = df.rename(
         columns={'#chr': 'chrom'}
     )
     if interval is not None:
         df = filter_df_to_interval(df, interval)
+    return df
+
+
+def df_to_genomic_intervals(df: pd.DataFrame, interval: GenomicInterval=None, extra_columns=()):
+    df = _sanitize_df(df, interval)
     result = [GenomicInterval(df_row['chrom'], df_row['start'], df_row['end'], **{col: df_row[col] for col in extra_columns}) for _, df_row in df.iterrows()]
     return result
 
 
-def df_to_variant_intervals(df, interval=None, extra_columns=()):
-    df = df.rename(
-        columns={'#chr': 'chrom'}
-    )
-    if interval is not None:
-        df = filter_df_to_interval(df, interval)
-    result = [VariantInterval(df_row['chrom'], df_row['start'], df_row['end'], ref=df_row['ref'], alt=df_row['alt'], **{col: df_row[col] for col in extra_columns}) for _, df_row in df.iterrows()]
+def df_to_variant_intervals(df: pd.DataFrame, interval: GenomicInterval=None, extra_columns=()):
+    df = _sanitize_df(df, interval)
+    result = [
+        VariantInterval(
+            df_row['chrom'],
+            df_row['start'],
+            df_row['end'],
+            ref=df_row['ref'],
+            alt=df_row['alt'],
+            **{col: df_row[col] for col in extra_columns}
+        ) for _, df_row in df.iterrows()
+    ]
     return result
+
+
+def genomic_intervals_to_df(genomic_intervals: List[GenomicInterval]):
+    assert len(genomic_intervals) > 0, "No intervals to convert"
+    req_fields = genomic_intervals[0].req_fields
+    extra_fields = genomic_intervals[0].extra_fields
+    assert all(inerval.extra_fields == extra_fields for inerval in genomic_intervals), "Not all _extra_fields of genomic_intervals are identical!"
+    fields = req_fields + extra_fields
+    return pd.DataFrame(
+        [
+            (getattr(interval, field) for field in fields)
+            for interval in genomic_intervals
+        ],
+        columns=fields
+    )
