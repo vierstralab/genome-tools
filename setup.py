cb0e63a54dec2e5392b555e8f607e2b063123aab
jvierstra <jvierstra@altius.org>
2025-07-22 20:16:01 -0700

    Add pyproject.toml

--- setup.py
+++ setup.py
@@ -28,15 +28,5 @@ setup(
 	packages =  find_packages(),
     install_requires = install_requires,
     download_url = "https://github.com/jvierstra/genome-tools/archive/v2.0.0.tar.gz",
-    classifiers=[
-	    'Development Status :: 5 - Production/Stable', 
-	    'Intended Audience :: Science/Research', 
-	    'Topic :: Scientific/Engineering :: Bio-Informatics',
-	    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-	    'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-		'Programming Language :: Python :: 3.8',
-		'Programming Language :: Python :: 3.9',
-],
+
 )
