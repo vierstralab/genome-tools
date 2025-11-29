48039752fd4359718630d575be680bade8f0e069
m1kss <boytsovs.av@gmail.com>
2025-11-28 16:08:34 -0800

    fix

--- genome_tools/plotting/__init__.py
+++ genome_tools/plotting/__init__.py
@@ -546,6 +546,7 @@ def segment_plot(interval: GenomicInterval, segments: List[GenomicInterval], rec
                     **interval_rectprops
                 )
             patches.append(arrow_patch)
+            print(arrow_patch)
 
         if hasattr(segment, "summit"):
             summit_lines.append(
