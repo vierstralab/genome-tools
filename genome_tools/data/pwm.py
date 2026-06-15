88fb61f32a38d7a12eef57b8e8119cb69ff03387
m1kss <boytsovs.av@gmail.com>
2026-06-15 13:40:25 -0700

    add delta_energy function and update plot_motif_logo to support multiple plot types

--- genome_tools/data/pwm.py
+++ genome_tools/data/pwm.py
@@ -35,11 +35,16 @@ def reverse_complement(seq):
     return "".join(_comp[b] for b in reversed(seq))
 
 
-def relative_info_content(pwm):
-    p = pwm / np.sum(pwm, axis=1)[:, np.newaxis]
+def relative_info_content(mat):
+    p = mat / np.sum(mat, axis=1)[:, np.newaxis]
     ic = 2 + np.sum(p * np.nan_to_num(np.log2(p)), axis=1)
     ric = p * ic[:, np.newaxis]
     return ric
+
+def delta_energy(mat):
+    p = mat / mat.sum(axis=1, keepdims=True)
+    energy = -np.log(p + 1e-3)
+    return energy - energy.mean(axis=1, keepdims=True)
 
 
 def seq_logp(mat, seq, bg=None, weights=None):
